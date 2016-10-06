98f692c04746b3093eade9fcb7ae6472a61c1c69
Joseph Luck <joseph.luck@sky.com>
2016-10-06 18:27:30 +0100

    Beginnings of templating more of the results elms

--- src/views/views.js
+++ src/views/views.js
@@ -1,32 +1,55 @@
 module.exports = {
   noResultsTemplate () {
     return `
-      <div>
+      <div class='no-results'>
         No results
       </div>
     `
   },
+  resultStatTemplate (resultStat, label) {
+    if (resultStat) {
+      return `
+        <div class='result-stat'>
+          <div class='result-stat-label'>
+            ${label}
+          </div>
+          <div class='result-stat-content'>
+            ${resultStat}
+          </div>
+        </div>
+      `
+    } else {
+      return ''
+    }
+  },
   resultTemplate (result) {
     return `
       <div class='result'>
-        ${result.original_title}
+        <div class='result-title'>
+          ${result.original_title}
+        </div>
+        <div class='result-description'>
+          ${result.overview}
+        </div>
+        <div class='result-stats'>
+          ${this.resultStatTemplate(result.release_date, 'Released on')}
+
+        </div>
       </div>
     `
   },
   renderScaffolding (elm) {
     elm.innerHTML = `
-      <div>
-        <div id='search-bar'>
-          <input id='search-input' />
-        </div>
-        <div id='results-list'>
-          ${this.noResultsTemplate()}
-        </div>
+      <div id='search-bar'>
+        <input id='search-input' />
+      </div>
+      <div id='results-list'>
+        ${this.noResultsTemplate()}
       </div>
     `
   },
   renderResults (elm, results) {
-    const resultsElm = results.map(this.resultTemplate).join('')
+    const resultsElm = results.map(this.resultTemplate.bind(this)).join('')
     elm.innerHTML = resultsElm
     this.removeLoading(elm)
   },
